6cf32dd50ff09dadcda48659e7559b3ca6146305
INTEGRAL\MSpencer <mspencer@integraltech.com.au>
2021-07-23 10:18:16 +1000

    added health endpoints

---
+++ src/main/java/org/acme/getting/started/health/SimpleLivenessCheck.java
@@ -0,0 +1,14 @@
+package org.acme.getting.started.health;
+
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Liveness;
+
+@Liveness
+public class SimpleLivenessCheck implements HealthCheck {
+
+    @Override
+    public HealthCheckResponse call() {
+        return HealthCheckResponse.up("Alive");
+    }
+}
